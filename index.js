c7f99bbe3e0d37cd6e8236bc49b4d0c641db6e1c
Redrouge64 <59136701+Redrouge64@users.noreply.github.com>
2019-12-22 11:27:38 +0100

    Add files via upload

---
+++ index.js
@@ -0,0 +1,38 @@
+const Discord = require('discord.js');
+const client = new Discord.Client();
+client.commands = new Discord.Collection();
+const fs = require('fs');
+
+fs.readdir('./Commandes/', (error, f) => {
+    if (error) { return console.error(error); }
+        let commandes = f.filter(f => f.split('.').pop() === 'js');
+        if (commandes.length <= 0) { return console.log('Aucune commande trouvée !'); }
+
+        commandes.forEach((f) => {
+            let commande = require(`./Commandes/${f}`);
+            console.log(`${f} commande chargée !`);
+            client.commands.set(commande.help.name, commande);
+        });
+
+});
+
+client.on('messageReactionAdd', (reaction, user) => {
+    if(reaction.emoji.name === "😍")
+        client.channels.get("658056592444358680").send('Bonjour');
+    client.channels.get("658056592444358680").send('Réaction ' + reaction.emoji.name);
+});
+
+
+
+fs.readdir('./Events/', (error, f) => {
+    if (error) { return console.error(error); }
+        console.log(`${f.length} events chargés`);
+
+        f.forEach((f) => {
+            let events = require(`./Events/${f}`);
+            let event = f.split('.')[0];
+            client.on(event, events.bind(null, client));
+        });
+});
+
+client.login('NjU3OTc0MzM1MTE3NTkwNTM1.Xf5eAA.VIL9ATaSEPRWhQn1JIgc6XGIt88');
