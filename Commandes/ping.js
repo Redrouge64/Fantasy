4b5b18e35eff8df8fb5eb7987d4bd36d82090095
Redrouge64 <59136701+Redrouge64@users.noreply.github.com>
2019-12-23 15:54:39 +0100

    Add files via upload

--- Commandes/ping.js
+++ Commandes/ping.js
@@ -1,14 +1,18 @@
-const Discord = require('discord.js');
+exports.run = async (client, message, args, level) => { // eslint-disable-line no-unused-vars
+  const msg = await message.channel.send("Ping?");
+  msg.edit(`Pong! Latency is ${msg.createdTimestamp - message.createdTimestamp}ms. API Latency is ${Math.round(client.ping)}ms`);
+};
 
-module.exports.run = (client, message, args) => {
-    let début = Date.now();
-    message.channel.send('Ping')
-        .then((m) => m.edit(`Pong : **${Date.now() - début}**ms`));
+exports.conf = {
+  enabled: true,
+  guildOnly: false,
+  aliases: [],
+  permLevel: "User"
 };
 
 exports.help = {
-    name: "Ping",
-    category: "System",
-    description: "Savoir son ping",
-    usage: "Roles"
-  };
+  name: "ping",
+  category: "Miscelaneous",
+  description: "It like... Pings. Then Pongs. And it's not Ping Pong.",
+  usage: "ping"
+};
